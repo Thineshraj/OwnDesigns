1d104789e2bf42a14cd552e724f69c44ed9f3d15
Thineshraj <thinesh7474@gmail.com>
2022-10-15 22:30:24 -0400

    update(form-validation): Add some extra features - Thineshraj

--- form-validation/script.js
+++ form-validation/script.js
@@ -1,34 +1,73 @@
 'use strict';
 
-const btn = document.querySelector('.btn-submit');
-const formSubmit = document.querySelector('#form-submit');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const button = document.querySelector('.btn');
 
-let validation = false;
+let usernameVal, emailVal, passVal;
 
-const formValidation = () => {
-  if (
-    username.value.trim() === '' ||
-    !email.value.includes('@') ||
-    password.value.length < 5
-  ) {
-    validation = false;
+username.addEventListener('input', usernameValidation);
+email.addEventListener('input', emailValidation);
+password.addEventListener('input', passwordValidation);
+button.addEventListener('mouseover', (e) => {
+  if (usernameVal && emailVal && passVal) {
+    button.className = 'btn btn-valid';
   } else {
-    validation = true;
+    button.className = 'btn invalidBtn';
   }
-};
+});
 
-btn.addEventListener('mouseover', function () {
-  formValidation();
-  if (validation) {
-    btn.classList.remove('invalid');
-    formSubmit.addEventListener('submit', (e) => {
-      e.preventDefault();
-      console.log(`Username: ${username.value}, Email: ${email.value}`);
-    });
-  } else {
-    btn.classList.add('invalid');
+const pass_reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+// Username Validation function
+function usernameValidation(e) {
+  if (e.target.name == 'username') {
+    if (e.target.value.length > 3) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+      usernameVal = true;
+    } else {
+      e.target.classList.add('invalid');
+      e.target.classList.remove('valid');
+      usernameVal = false;
+    }
   }
+}
+
+// Email validation function
+function emailValidation(e) {
+  if (e.target.name == 'email') {
+    if (e.target.value.includes('@')) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+      emailVal = true;
+    } else {
+      e.target.classList.add('invalid');
+      e.target.classList.remove('valid');
+      emailVal = false;
+    }
+  }
+}
+
+// Password validation function
+function passwordValidation(e) {
+  if (e.target.name == 'password') {
+    if (pass_reg.test(e.target.value)) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+      passVal = true;
+    } else {
+      e.target.classList.add('invalid');
+      e.target.classList.remove('valid');
+      passVal = false;
+    }
+  }
+}
+
+// Form submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(`Username: ${username.value}, Email: ${email.value}`);
 });
